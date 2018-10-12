ffe7d6be9e0e2e24c77ec2c477791b413a31df49
IngDev12 <IngDev12@IngSR.AISYTEK.COM>
2018-10-12 16:49:08 +0100

    Build error myTest 999

--- Documents/Angular5Projects/server/src/main/java/com/aisyhire/server/ServerApplication.java
+++ Documents/Angular5Projects/server/src/main/java/com/aisyhire/server/ServerApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import java.util.stream.Stream;
+
+
+
+
+
 /**
  *
  * @author Mehdi Benkirane TEST
